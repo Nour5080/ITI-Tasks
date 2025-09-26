72c517ec41508f213062d1c2eaaffacd34b35510
Nour Mohamed <161865218+Nour5080@users.noreply.github.com>
2025-09-26 20:55:50 +0300

    Add files via upload

---
+++ EF Day2/Models/student.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace SchoolApp.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Major { get; set; }
+
+        public virtual ICollection<StudentCourse> StudentCourses { get; set; } = new List<StudentCourse>();
+    }
+}
